068704d00343b40acbbacb1f0aab598f05b1d98d
Hermas <hermas.yuda@gmail.com>
2020-03-21 13:46:28 +0700

    adding data class hero

---
+++ avengersapp/app/src/main/java/com/catnip/avengersapp/data/model/Hero.kt
@@ -0,0 +1,14 @@
+package com.catnip.avengersapp.data.model
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+
+data class Hero(val name: String, val desc: String, val imgPath: List<String>)
+
+
+
+object HeroGenerator{
+
+}
